c2840ebb2ddf89048fdf905cd0e6c46dc6062a86
communistWatermelon <zyko101@gmail.com>
2014-03-10 14:21:48 -0700

    Removed useless fucntions from last assignment

--- epollServer.c
+++ epollServer.c
@@ -55,6 +55,7 @@ typedef struct {
 
 //Globals
 int fd_server;
+int websocket;
 epollWrapper info;
 
 // Function prototypes
@@ -69,6 +70,36 @@ void setupFD(struct epoll_event * event);
 int handleConnection(struct epoll_event * event, int * i);
 void handleData(int * i);
 
+void connectToWebserver()
+{
+    struct hostent *hp;
+	struct sockaddr_in server;
+	char  *host;
+
+    if ((websocket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		perror("Cannot create socket");
+		exit(1);
+	}
+    
+    bzero((char *)server, sizeof(struct sockaddr_in));
+	server->sin_family = AF_INET;
+	server->sin_port = htons(80);
+	if ((hp = gethostbyname(host)) == NULL)
+	{
+		fprintf(stderr, "Unknown server address\n");
+		exit(1);
+	}
+	bcopy(hp->h_addr, (char *)&server->sin_addr, hp->h_length);
+    
+	if (connect (*sd, (struct sockaddr *)server, sizeof(*server)) == -1)
+	{
+		fprintf(stderr, "Can't connect to server\n");
+		perror("connect");
+		exit(1);
+	}
+
+}
 
 /*------------------------------------------------------------------------------
 --
@@ -94,10 +125,11 @@ int main (int argc, char* argv[])
 	static struct epoll_event event;
 	int port = SERVER_PORT;
 
-    setupSignal();
-   
-	setupListenSocket(port);
 
+    connectToWebserver();
+
+    setupSignal();
+	setupListenSocket(port);
 	setupFD(&event);
     
 	// Execute the epoll event loop
